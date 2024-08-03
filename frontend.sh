42fbb286ec485b368600d2b1bdfbec6011b43b2c
NeelaReddy <neelareddy.i25@gmail.com>
2024-08-03 22:59:00 +0530

    good effects

--- frontend.sh
+++ frontend.sh
@@ -46,9 +46,6 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
-cp /home/ec2-user/expense-shell/nginx.conf /etc/nginx/nginx.conf &>>$LOGFILE
-VALIDATE $? "Updating nginx conf"
-
 #Check your repo and path
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
